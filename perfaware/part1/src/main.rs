67f0138d2b1b84ba9f9e995df6b9b04b5b039a59
Cypress Frankenfeld <iamcypress@gmail.com>
2025-05-21 22:46:42 -0400

    Make binary easier to read

--- perfaware/part1/src/main.rs
+++ perfaware/part1/src/main.rs
@@ -22,11 +22,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         let opcode = byte >> 2;
 
         let line = match opcode >> 2 {
-            0b1011 => {
+            0b_1011 => {
                 // immediate mov
                 let operation = "mov";
-                let w = (byte & 0b0000_1000) >> 3;
-                let reg = byte & 0b0000_0111;
+                let w = (byte & 0b_0000_1000) >> 3;
+                let reg = byte & 0b_0000_0111;
                 let data: u16 = match w {
                     0 => *bytes.next().unwrap() as u16,
                     1 => {
@@ -38,71 +38,73 @@ fn main() -> Result<(), Box<dyn Error>> {
                 let register = register_for(reg, w);
                 format!("{operation} {register}, {data}")
             }
-            _ => match opcode {
-                0b100010 => {
-                    // reg/reg or reg/mem mov
-                    let operation = "mov";
-                    let d = (byte & 0b0000_0010) >> 1;
-                    let w = byte & 0b0000_0001;
+            _ => {
+                match opcode {
+                    0b_100010 => {
+                        // reg/reg or reg/mem mov
+                        let operation = "mov";
+                        let d = (byte & 0b_0000_0010) >> 1;
+                        let w = byte & 0b_0000_0001;
 
-                    let byte = bytes.next().expect("no second byte for reg/mem mov");
-                    let mod_ = byte >> 6;
-                    let reg = (byte & 0b0011_1000) >> 3;
-                    let rm = byte & 0b0000_0111;
+                        let byte = bytes.next().expect("no second byte for reg/mem mov");
+                        let mod_ = byte >> 6;
+                        let reg = (byte & 0b_0011_1000) >> 3;
+                        let rm = byte & 0b_0000_0111;
 
-                    let (reg, rm) = match mod_ {
-                        0b00 | 0b01 | 0b10 => {
-                            let address_calculation = {
-                                let expression = match rm {
-                                    0b000 => "bx + si",
-                                    0b001 => "bx + di",
-                                    0b010 => "bp + si",
-                                    0b011 => "bp + di",
-                                    0b100 => "si",
-                                    0b101 => "di",
-                                    0b110 => {
-                                        if mod_ == 0b00 {
-                                            &bytes.next().unwrap().to_string()
-                                        } else {
-                                            "bp"
+                        let (reg, rm) = match mod_ {
+                            0b_00 | 0b_01 | 0b_10 => {
+                                let address_calculation = {
+                                    let expression = match rm {
+                                        0b_000 => "bx + si",
+                                        0b_001 => "bx + di",
+                                        0b_010 => "bp + si",
+                                        0b_011 => "bp + di",
+                                        0b_100 => "si",
+                                        0b_101 => "di",
+                                        0b_110 => {
+                                            if mod_ == 0b_00 {
+                                                &bytes.next().unwrap().to_string()
+                                            } else {
+                                                "bp"
+                                            }
                                         }
-                                    }
-                                    0b111 => "bx",
-                                    _ => panic!("invalid reg {rm:b} must be 3 bits"),
-                                };
-                                let displacement: u16 = match mod_ {
-                                    0b00 => 0,
-                                    0b01 => *bytes.next().unwrap() as u16,
-                                    0b10 => u16::from_le_bytes([
-                                        *bytes.next().unwrap(),
-                                        *bytes.next().unwrap(),
-                                    ]),
-                                    _ => {
-                                        panic!("invalid mod {mod_:b} must be between 0b00 and 0b10")
-                                    }
-                                };
+                                        0b_111 => "bx",
+                                        _ => panic!("invalid reg {rm:b} must be 3 bits"),
+                                    };
+                                    let displacement: u16 = match mod_ {
+                                        0b_00 => 0,
+                                        0b_01 => *bytes.next().unwrap() as u16,
+                                        0b_10 => u16::from_le_bytes([
+                                            *bytes.next().unwrap(),
+                                            *bytes.next().unwrap(),
+                                        ]),
+                                        _ => {
+                                            panic!("invalid mod {mod_:b} must be between 0b_00 and 0b_10")
+                                        }
+                                    };
 
-                                if displacement != 0 {
-                                    format!("[{expression} + {displacement}]")
-                                } else {
-                                    format!("[{expression}]")
-                                }
-                            };
-                            (register_for(reg, w).into(), address_calculation)
-                        }
-                        0b11 => (register_for(reg, w).into(), register_for(rm, w).into()),
-                        _ => panic!("mod must be 2 bits, but was {mod_:b}"),
-                    };
+                                    if displacement != 0 {
+                                        format!("[{expression} + {displacement}]")
+                                    } else {
+                                        format!("[{expression}]")
+                                    }
+                                };
+                                (register_for(reg, w).into(), address_calculation)
+                            }
+                            0b_11 => (register_for(reg, w).into(), register_for(rm, w).into()),
+                            _ => panic!("mod must be 2 bits, but was {mod_:b}"),
+                        };
 
-                    let (destination, source) = match d {
-                        0 => (rm, reg),
-                        1 => (reg, rm),
-                        _ => panic!("d must be 0 or 1, but was {d:b}"),
-                    };
-                    format!("{operation} {destination}, {source}")
+                        let (destination, source) = match d {
+                            0 => (rm, reg),
+                            1 => (reg, rm),
+                            _ => panic!("d must be 0 or 1, but was {d:b}"),
+                        };
+                        format!("{operation} {destination}, {source}")
+                    }
+                    _ => todo!("unknown opcode {opcode:b}"),
                 }
-                _ => todo!("unknown opcode {opcode:b}"),
-            },
+            }
         };
         output_file.write_all((line + "\n").as_bytes())?;
     }
@@ -111,25 +113,25 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn register_for(reg: u8, w: u8) -> &'static str {
     match (reg, w) {
-        (0b000, 0) => "al",
-        (0b001, 0) => "cl",
-        (0b010, 0) => "dl",
-        (0b011, 0) => "bl",
+        (0b_000, 0) => "al",
+        (0b_001, 0) => "cl",
+        (0b_010, 0) => "dl",
+        (0b_011, 0) => "bl",
 
-        (0b100, 0) => "ah",
-        (0b101, 0) => "ch",
-        (0b110, 0) => "dh",
-        (0b111, 0) => "bh",
+        (0b_100, 0) => "ah",
+        (0b_101, 0) => "ch",
+        (0b_110, 0) => "dh",
+        (0b_111, 0) => "bh",
 
-        (0b000, 1) => "ax",
-        (0b001, 1) => "cx",
-        (0b010, 1) => "dx",
-        (0b011, 1) => "bx",
+        (0b_000, 1) => "ax",
+        (0b_001, 1) => "cx",
+        (0b_010, 1) => "dx",
+        (0b_011, 1) => "bx",
 
-        (0b100, 1) => "sp",
-        (0b101, 1) => "bp",
-        (0b110, 1) => "si",
-        (0b111, 1) => "di",
+        (0b_100, 1) => "sp",
+        (0b_101, 1) => "bp",
+        (0b_110, 1) => "si",
+        (0b_111, 1) => "di",
 
         _ => panic!("Not valid binary"),
     }
